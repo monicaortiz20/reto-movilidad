520d155071b6b6871aa40cfc8ac0f55c1dacc8bb
Sofía Guardia Guillan <sofiamguardia@gmail.com>
2022-10-15 01:35:17 +0200

    add/ conflicts solved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
-import {BrowserRouter} from 'react-router-dom';
-=======
 import  { BrowserRouter } from 'react-router-dom';
 
->>>>>>> ef5123897bee28adbd4ac613bafce899d55b586f
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
@@ -13,18 +9,11 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-<<<<<<< HEAD
-        <Header />
-        <Main />
-        <Footer />
-      </BrowserRouter>
-=======
       <Header/>
       <Main/>
       <Footer/>
       </BrowserRouter>
 
->>>>>>> ef5123897bee28adbd4ac613bafce899d55b586f
     </div>
   )
 }
