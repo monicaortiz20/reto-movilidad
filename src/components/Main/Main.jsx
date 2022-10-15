ba3554c9a1732253db45266f1bb8c01f81b4e1dd
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-10-15 11:50:34 +0200

    conflicts slolved

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import React from 'react'
 import {Route,Routes} from 'react-router-dom'
+
 import Home from "./Home/Home"
 import Profile from './Profile/Profile'
 
@@ -12,23 +12,6 @@ const Main = () => {
       <Route element={<Profile />} path={"/profile"} />
     </Routes>
   </main>
-=======
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-
-import Home from '../Main/Home/Home';
-import Profile from '../Main/Profile/Profile';
-
-
-const Main = () => {
-  return (
-    <main>
-      <Routes>
-        <Route element={<Home/>} path={'/'}>Home</Route>
-        <Route element={<Profile/>} path={'/profile'}>Profile</Route>
-      </Routes>
-    </main>
->>>>>>> ef5123897bee28adbd4ac613bafce899d55b586f
   )
 }
 
