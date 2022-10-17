113c6c3663084d834a0bb762a6471ad87a15bee9
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-10-17 22:08:20 +0200

    ADD/ merge develop

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
-// import { getStorage, ref, uploadBytes, getDownloadURL, getBytes } from 'firebase/storage';
-// import { getFirestore, collection, addDoc, getDocs, doc, getDoc, query, where, setDoc, deleteDoc } from 'firebase/firestore';
+// import { getStorage, ref, uploadBytes, getDownloadURL, getBytes } from 'firebase/storage';//fotos perfil
+import { getFirestore, collection, addDoc, getDocs, doc, getDoc, query, where, setDoc, deleteDoc } from 'firebase/firestore';//users collection
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY_FIREBASE,
@@ -18,13 +18,13 @@ const firebaseConfig = {
  export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-// const db = getFirestore(app);
+export const db = getFirestore(app);
 // const storage = getStorage(app)
 
 
-// export async function userExists(uid){
-//   const docRef = doc(db,'users', uid);
-//   const res = await getDoc(docRef)
-//   console.log(res)
-//   return res.exists();
-// }
+export async function userExists(uid){
+  const docRef = doc(db,'users', uid);
+  const res = await getDoc(docRef)
+  console.log(res)
+  return res.exists();
+}
