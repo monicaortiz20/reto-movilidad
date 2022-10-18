cb44e02593d597ab757b631f4f9ab7fa05eaa194
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-10-18 18:25:21 +0200

    ADD/merge firebase auth + firestore

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,11 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 
-// import { getFirestore, collection, addDoc, getDocs, doc, getDoc, query, where, setDoc, deleteDoc } from 'firebase/firestore';
-
-//import { getStorage, ref, uploadBytes, getDownloadURL, getBytes } from 'firebase/storage';
-
-
 const firebaseConfig = { 
   apiKey: process.env.REACT_APP_APIKEY_FIREBASE,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
@@ -20,28 +15,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-
-
-
-// export const db = getFirestore(app);  
-// const storage = getStorage(app)
-
-//para saber si el usuario existe
-// export async function userExists(uid){
-//   const docRef = doc(db,'users', uid);
-//   const res = await getDoc(docRef)
-//   console.log(res)
-//   return res.exists();
-// }
-
-// //para registrar usuario
-// export async function registerNewUser(user){
-//   try {
-//     const collectionRef = collection(db, 'users');
-//     const docRef = doc(collectionRef, user.uid)
-//     await setDoc(docRef, user);
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
 
